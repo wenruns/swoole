24429c0d1feb554f9d9a1ffdce58ecbb44a1ae29
J.tart <942242759@qq.com>
2019-08-21 18:09:25 +0800

    add

--- public/game/src/components/home.vue
+++ public/game/src/components/home.vue
@@ -13,37 +13,107 @@
         />
         <div class="pride-box">
             <div class="pride-title">最新中奖榜</div>
-            <van-swipe style="height: 10vw;" vertical :autoplay="3000" :show-indicators="false">
-                <van-swipe-item class="pride-one-box">
-                    <div class="pride-one-box">
-                        <span>程序**</span>
-                        <span>购买大富科技</span>
-                        <span>喜中1002.00</span>
+            <swiper-text :list="options"></swiper-text>
+        </div>
+        <div class="game">
+            <div class="game-title">热门游戏</div>
+            <div class="game-list-box">
+                <div class="one-game-box" @click="toGame1">
+                    <div class="one-game-left one-game-info">
+                        <div class="game-name-box">大富科技</div>
+                        <div class="game-tips-box">距离下次开奖剩余</div>
+                        <div class="game-time-box">12:12</div>
                     </div>
-                </van-swipe-item>
-                <van-swipe-item class="pride-one-box">
-                    <div class="pride-one-box">
-                        <span>程序**</span>
-                        <span>购买大富科技</span>
-                        <span>喜中1002.00</span>
+                    <div class="one-game-right"></div>
+                </div>
+                <div class="one-game-box" @click="toGame2">
+                    <div class="one-game-left"></div>
+                    <div class="one-game-right one-game-info">
+                        <div class="game-name-box">卓翼科技</div>
+                        <div class="game-tips-box">等待开奖中</div>
+                        <div class="game-time-box">{{timeCount()}}:12</div>
                     </div>
-                </van-swipe-item>
-            </van-swipe>
+                </div>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
     import { Swipe, SwipeItem, NoticeBar } from 'vant'
+    import swiperText from '@/components/seconds/swiperText'
     export default {
         name: 'home',
         components:{
+            swiperText,
             [Swipe.name]: Swipe,
             [SwipeItem.name]: SwipeItem,
             [NoticeBar.name]: NoticeBar,
         },
         data(){
             return {
+                options:[
+                    {
+                        left: {
+                            text: '红行**',
+                            style: 'font-size:4vw !important;'
+                        },
+                        center: {
+                            text: '购买大富科技',
+                            style: 'font-size:4vw !important;'
+                        },
+                        right: {
+                            text: '喜中100.01',
+                            style: 'color:red;font-size: 4vw !important;'
+                        },
+                    },
+                    {
+                        left: {
+                            text: '小溪**',
+                            style: 'font-size:4vw !important;'
+                        },
+                        center: {
+                            text: '购买科技以强',
+                            style: 'font-size:4vw !important;'
+                        },
+                        right: {
+                            text: '喜中100.01',
+                            style: 'color:red;font-size:4vw !important;'
+                        }
+                    },
+                    {
+                        left: {
+                            text: '吸血**',
+                            style: 'font-size:4vw !important;'
+
+                        },
+                        center: {
+                            text: '购买嘻嘻嘻',
+                            style: 'font-size:4vw !important;'
+
+                        },
+                        right: {
+                            text: '喜中100.01',
+                            style: 'color:red;font-size:4vw !important;'
+                        }
+                    },
+                    {
+                        left: {
+                            text: '青溪**',
+                            style: 'font-size:4vw !important;'
+
+                        },
+                        center: {
+                            text: '购买双宿双飞',
+                            style: 'font-size:4vw !important;'
+
+                        },
+                        right: {
+                            text: '喜中100.01',
+                            style: 'color:red;font-size:4vw !important;'
+                        }
+                    }
+                ],
                 notices: [
                     "通知内容山东矿机了大姐夫打卡的，看到了放款山东矿机思科的螺丝孔拉开距离卡快递费就连狂阶地方",
                     "通知内容山东矿机了大姐夫打卡的，看到了放款山东矿机思科的螺丝孔拉开距离卡快递费就连狂阶地方",
@@ -58,12 +128,35 @@
                         url: '',
                         img_src: 'https://img.yzcdn.cn/vant/apple-2.jpg'
                     }
-                ]
+                ],
+                next_time: {
+                    game1: 123,
+                    game2: 1234
+                }
             }
         },
-        beforeCreate() {
+        created(){
+            this.timeCount(133);
         },
         methods:{
+            toGame1(){
+
+            },
+            toGame2(){
+
+            },
+            timeCount(time, flag=0){
+                if(flag == 0){
+                    let h = Math.floor(time/3600);
+                    return h;
+                }
+                let m = Math.floor(time/60);
+                if(flag == 1){
+                    return m;
+                }
+                let s = (time%3600)%60;
+                return s;
+            }
         },
         computed:{
             notice_content(){
@@ -76,7 +169,6 @@
         }
     }
 </script>
-
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
     .swiper-image{
@@ -86,19 +178,64 @@
     .pride-box{
         background: white;
     }
-    .pride-title{
+    .pride-title,.game-title{
         height: 10vw;
         line-height: 10vw;
         color: red;
     }
-    .pride-one-box{
+    .game-list-box{
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+        align-items: center;
+    }
+    .one-game-box{
         display: flex;
         justify-content: space-between;
+        align-items: center;
+        width: 90vw;
+        height: 40vw;
+        background: red;
+        -webkit-border-radius: 2vw;
+        -moz-border-radius: 2vw;
+        border-radius: 2vw;
+        margin-bottom: 3vw;
+        color: white;
+    }
+    .one-game-left,.one-game-right{
+        text-align: left;
+    }
+    .one-game-right{
+    }
+    .one-game-info{
+        width: 50vw;
         -webkit-box-sizing: border-box;
         -moz-box-sizing: border-box;
         box-sizing: border-box;
-        padding: 0px 2vw;
-        height: 10vw !important;
-        line-height: 10vw;
+        padding: 3vw;
+    }
+    .game-name-box{
+        font-size: 10.5vw !important;
+        font-weight: bold;
+    }
+    .one-game-right .game-name-box{
+        text-align: right;
+    }
+    .game-tips-box{
+        font-size: 4.5vw !important;
+        -webkit-box-sizing: border-box;
+        -moz-box-sizing: border-box;
+        box-sizing: border-box;
+        padding: 1vw 2vw;
+    }
+    .game-time-box{
+        font-size: 4.5vw !important;
+        -webkit-box-sizing: border-box;
+        -moz-box-sizing: border-box;
+        box-sizing: border-box;
+        padding: 1vw 2vw;
+    }
+    .one-game-right .game-tips-box,.one-game-right .game-time-box{
+        padding: 1vw 4vw;
     }
 </style>
---
+++ public/game/src/components/seconds/swiperText.vue
@@ -0,0 +1,195 @@
+<template>
+    <div class="swipe-text-box">
+        <ul :style="'height: '+height+';'">
+            <template v-for="(item, index) in options">
+                <li :class="class_name" :style="'height: '+li_height(0)+';line-height:' + li_height(0)+';top:'+(li_height(1)*(index))+li_height(2)+';'+item.style" :data-index="index" :data-flag="index">
+                    <span :style="item.left.style">{{item.left.text}}</span>
+                    <span :style="item.center.style">{{item.center.text}}</span>
+                    <span :style="item.right.style">{{item.right.text}}</span>
+                </li>
+            </template>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "swiperText",
+        props: {
+            row: {
+                type: Number,
+                default: 3,
+            },
+            height: {
+                type: String,
+                default: '20vw'
+            },
+            autoplay: {
+                type: Number,
+                default: 1000
+            },
+            speed: {
+                type: Number,
+                default: 500
+            },
+            list: {
+                type: [Array, Object],
+                default: function () {
+                    return [
+                        {
+                            left: {
+                                text: 'ab**',
+                                style: '',
+                            },
+                            center: {
+                                text: '嘎嘎嘎嘎嘎',
+                                style: '',
+                            },
+                            right: {
+                                text: '123123',
+                                style: ''
+                            },
+                            style: ''
+                        },
+                        {
+                            left: {
+                                text: 'ab**',
+                                style: '',
+                            },
+                            center: {
+                                text: '嘎嘎嘎嘎嘎',
+                                style: '',
+                            },
+                            right: {
+                                text: '123123',
+                                style: ''
+                            },
+                            style: ''
+                        },
+                        {
+                            left: {
+                                text: 'ab**',
+                                style: '',
+                            },
+                            center: {
+                                text: '嘎嘎嘎嘎嘎',
+                                style: '',
+                            },
+                            right: {
+                                text: '123123',
+                                style: ''
+                            },
+                            style: ''
+                        }
+                    ]
+                }
+            }
+        },
+        data(){
+            return {
+                handle: null,
+                options: [],
+                index: 0,
+                class_name: '',
+                windowIsOnFocus: true
+            }
+        },
+        created(){
+            this.class_name = 'swipe-text-box-'+Math.round(Math.random()*10000);
+            let i = 0, j = 0;
+            for(; i <= this.row; i++, j++){
+                if(j >= this.list.length){
+                    j = 0;
+                }
+                this.options.push(this.list[j]);
+            }
+            this.index = j;
+        },
+        mounted() {
+            let _this = this;
+            let lis = document.querySelectorAll('.swipe-text-box .'+this.class_name);
+            let hiddenProperty = 'hidden' in document ? 'hidden' :
+                'webkitHidden' in document ? 'webkitHidden' :
+                    'mozHidden' in document ? 'mozHidden' :
+                        null;
+            // 不同浏览器的事件名
+            let visibilityChangeEvent = hiddenProperty.replace(/hidden/i, 'visibilitychange');
+            document.addEventListener(visibilityChangeEvent, () => {
+                if (!document[hiddenProperty]) {
+                    _this.windowIsOnFocus = true;
+                    // document.title = '页面激活';
+                }else{
+                    _this.windowIsOnFocus = false;
+                    // document.title='页面非激活';
+                }
+            });
+            lis.forEach(function (item, index) {
+                item.addEventListener('transitionend', function (e) {
+                    if(item.dataset.flag == (_this.row)){
+                        item.style.transition = '';
+                        item.style.top = (Number(this.dataset.flag)) * _this.li_height(1) + _this.li_height(2);
+                        if(_this.index >= _this.list.length){
+                            _this.index = 0;
+                        }
+                        _this.options[item.dataset.index] = _this.list[_this.index];
+                        _this.index++;
+                        _this.$forceUpdate();
+                    }
+
+                })
+            })
+            this.handle = setInterval(function (e) {
+                if(_this.windowIsOnFocus){
+                        lis.forEach(function (item, index) {
+                        item.style.transition = 'all '+_this.speed+'ms';
+                        item.style.top = _this.li_height(1)*(item.dataset.flag-1)+_this.li_height(2);
+                        item.dataset.flag--;
+                        if(item.dataset.flag < 0){
+                            item.dataset.flag = _this.row;
+                        }
+                    })
+                }
+            }, this.autoplay+this.speed)
+        },
+        beforeDestroy(){
+            clearInterval(this.handle)
+        },
+        methods:{
+            li_height(flag = 0){
+                let h = /([0-9]+)(\S+)/.exec(this.height);
+                let lh = h[1];
+                let px = h[2];
+                if(flag == 0){
+                    return lh/this.row + px;
+                }else if(flag == 1){
+                    return lh/this.row;
+                }else{
+                    return px;
+                }
+            },
+        }
+    }
+</script>
+
+<style scoped>
+    .swipe-text-box{
+        padding: 2vw;
+    }
+    ul{
+        position: relative;
+    }
+    ul,li{
+        width: 100%;
+        list-style: none;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        /*text-overflow-ellipsis: '...';*/
+    }
+    li{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        position: absolute;
+        will-change: transition;
+    }
+</style>
